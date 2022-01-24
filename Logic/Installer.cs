ec5ddf03d069beb915175999772652824f9d62fa
artem-fomin-by <artem.fomin.by@gmail.com>
2022-01-24 21:57:36 +0300

    Added Installer and Remover

---
+++ Logic/Installer.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Win32;
+
+namespace Logic{
+    public static class Installer{
+        public static void Install(string name){
+
+        }
+    }
+}
---
+++ Logic/Remover.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace Logic{
+    public static class Remover{
+        public static void Remove(string name){
+
+        }
+    }
+}
