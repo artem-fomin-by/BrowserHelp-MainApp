45c6050bc386b5804845ef13e88e81ecc7b3020f
artem-fomin-by <artem.fomin.by@gmail.com>
2022-01-24 21:46:41 +0300

    Added BrowserButton in WinFormsLogic
    Removed WPF MainApp

---
+++ WinFormsLogic/BrowserButton.cs
@@ -0,0 +1,24 @@
+﻿using Logic.BrowserLogic;
+
+namespace WinFormsLogic{
+    public class BrowserButton : Button{
+        private readonly Browser BrowserToLaunch;
+        private readonly string Link;
+
+        private void LaunchBrowser(object? sender, EventArgs args){
+            BrowserToLaunch.Launch(Link);
+            Application.Exit();
+        }
+
+        public BrowserButton(Browser browser, string link){ 
+            Name = browser.Name;
+            BrowserToLaunch = browser;
+            Link = link;
+            Click += LaunchBrowser;
+        }
+
+        public void Initialise(int x, int y){
+
+        }
+    }
+}
