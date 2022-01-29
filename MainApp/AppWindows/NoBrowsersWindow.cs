f8935d96f8e1070b20200b07e535d4a7972b8783
artem-fomin-by <artem.fomin.by@gmail.com>
2022-01-29 15:05:04 +0300

    Added NoBrowsersWindow

---
+++ MainApp/AppWindows/NoBrowsersWindow.cs
@@ -0,0 +1,14 @@
+﻿namespace MainApp.AppWindows{
+    public partial class NoBrowsersWindow : Form{
+        public NoBrowsersWindow(string name){
+            InitializeComponent();
+
+            button1.Click += (sender, args) => {
+                Application.Exit();
+            };
+
+            Name = name;
+            Text = name;
+        }
+    }
+}
