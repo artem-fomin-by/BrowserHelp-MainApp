163f2afb6360520fd6b8a5a2baa6a202cab744ea
artem-fomin-by <artem.fomin.by@gmail.com>
2022-01-29 18:21:22 +0300

    Written code for ErrorWindow

--- MainApp/AppWindows/ErrorWindow.Designer.cs
+++ MainApp/AppWindows/ErrorWindow.Designer.cs
@@ -28,12 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 10);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(146, 87);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "An error has aquired during the execution of the program";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(165, 43);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(125, 120);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.Location = new System.Drawing.Point(165, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Error description";
+            // 
+            // ErrorWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(302, 177);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "ErrorWindow";
             this.Text = "ErrorWindow";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button button1;
+        private RichTextBox richTextBox1;
+        private RichTextBox richTextBox2;
+        private TextBox textBox1;
     }
 }
--- MainApp/AppWindows/ErrorWindow.cs
+++ MainApp/AppWindows/ErrorWindow.cs
@@ -2,6 +2,15 @@
     public partial class ErrorWindow : Form{
         public ErrorWindow(string name, Exception exception){
             InitializeComponent();
+
+            richTextBox2.Text = exception.Message;
+
+            button1.Click += (sender, args) => {
+                Application.Exit();
+            };
+
+            Name = name;
+            Text = name;
         }
     }
 }
