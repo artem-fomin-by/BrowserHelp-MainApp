c4c2f734e0a5ca9da8ba3ef7de6fa5ee46f88d23
Fomin Artem <artem.fomin.by@gmail.com>
2022-07-11 13:06:33 +0300

    Added wildcard Logic

---
+++ Logic/WildcardUtilities.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Logic;
+
+public static class WildcardUtilities
+{
+    public static Regex WildcardToRegex(string wildcard)
+    {
+        var regexPattern = Regex.Escape(wildcard).Replace("\\*", ".*");
+        return new Regex(regexPattern);
+    }
+}
---
+++ MainProgramTests/WildcardTests.cs
@@ -0,0 +1,36 @@
+﻿
+
+using Logic;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+
+namespace MainProgramTests;
+
+internal class WildcardTests
+{
+    private static bool HasPreviousTestPassed = true;
+
+    [SetUp]
+    public void SetUp()
+    {
+        Assume.That(HasPreviousTestPassed, Is.True);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        if(HasPreviousTestPassed)
+        {
+            HasPreviousTestPassed = TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Passed;
+        }
+    }
+
+    [Test]
+    public void ConvertingTest()
+    {
+        var regexPattern = @".*\.youtube.com/.*";
+        var wildcard = @"*.youtube.com/*";
+        var wildcardRegex = WildcardUtilities.WildcardToRegex(wildcard);
+        Assert.AreEqual(regexPattern, wildcardRegex.ToString());
+    }
+}
